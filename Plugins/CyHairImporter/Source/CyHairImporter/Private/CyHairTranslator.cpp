2482fa803cd4082c639d587d2e9f099942b9cd94
TienAska <constantine.sky@hotmail.com>
2022-02-17 18:32:05 +0800

    Update hair translator.
    Importer support thichness.

--- Plugins/CyHairImporter/Source/CyHairImporter/Private/CyHairTranslator.cpp
+++ Plugins/CyHairImporter/Source/CyHairImporter/Private/CyHairTranslator.cpp
@@ -27,22 +27,49 @@ bool FCyHairTranslator::Translate(const FString& FilePath, FHairDescription& Out
 	
 	// Parse Object.
 	{
-		uint32 pointIndex = 0;
 		const uint16* segments = hairFile.GetSegmentsArray();
-		for (size_t hairIndex = 0; hairIndex < hairCount; pointIndex += segments[hairIndex++] + 1)
+		const float* points = hairFile.GetPointsArray();
+		for (size_t pointIndex = 0, hairIndex = 0; hairIndex < hairCount; hairIndex++)
 		{
-			const float* points = hairFile.GetPointsArray();
+			int vertexCount = (segments ? segments[hairIndex] : hairFile.GetHeader().d_segments) + 1;
 
 			FStrandID strandID = OutHairDescription.AddStrand();
-			SetHairStrandAttribute(OutHairDescription, strandID, HairAttribute::Strand::VertexCount, segments[hairIndex] + 1);
-			for (size_t i = pointIndex; i < pointIndex + segments[hairIndex] + 1; i++)
+			SetHairStrandAttribute(OutHairDescription, strandID, HairAttribute::Strand::VertexCount, vertexCount);
+			for (size_t i = pointIndex; i < pointIndex + vertexCount; i++)
 			{
 				FVertexID vertexID = OutHairDescription.AddVertex();
 				FVector position(points[i * 3], points[i * 3 + 1], points[i * 3 + 2]);
-
 				SetHairVertexAttribute(OutHairDescription, vertexID, HairAttribute::Vertex::Position, position);
 			}
+			pointIndex += vertexCount;
 		}
+
+		TVertexAttributesRef<float> vertexWidthArributeRef = OutHairDescription.VertexAttributes().GetAttributesRef<float>(HairAttribute::Vertex::Width);
+		if (!vertexWidthArributeRef.IsValid())
+		{
+			OutHairDescription.VertexAttributes().RegisterAttribute<float>(HairAttribute::Vertex::Width);
+			vertexWidthArributeRef = OutHairDescription.VertexAttributes().GetAttributesRef<float>(HairAttribute::Vertex::Width);
+		}
+		const float* thicknesses = hairFile.GetThicknessArray();
+		for (size_t i = 0; i < hairFile.GetHeader().point_count; i++)
+		{
+			float thickness = thicknesses ? thicknesses[i] : hairFile.GetHeader().d_thickness;
+			vertexWidthArributeRef[FVertexID(i)] = thickness;
+		}
+
+		//TVertexAttributesRef<FVector> vertexColorArributeRef = OutHairDescription.VertexAttributes().GetAttributesRef<FVector>(HairAttribute::Vertex::Color);
+		//if (!vertexColorArributeRef.IsValid())
+		//{
+		//	OutHairDescription.VertexAttributes().RegisterAttribute<FVector>(HairAttribute::Vertex::Width);
+		//	vertexColorArributeRef = OutHairDescription.VertexAttributes().GetAttributesRef<FVector>(HairAttribute::Vertex::Color);
+		//}
+		//float* colors = hairFile.GetColorsArray();
+		//for (size_t i = 0; i < hairFile.GetHeader().point_count; i++)
+		//{
+		//	FVector color(hairFile.GetHeader().d_color[0], hairFile.GetHeader().d_color[1], hairFile.GetHeader().d_color[2]);
+		//	if (colors) color.Set(colors[i * 3], colors[i * 3 + 1], colors[i * 3 + 2]);
+		//	vertexColorArributeRef[FVertexID(i)] = color;
+		//}
 	}
 
 	return true;
